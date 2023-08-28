70c6cd764d1fd0dac513532689910ba9c29961a4
MNurIkbal <ikbalmnur17@gmail.com>
2023-08-28 09:43:08 +0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam minus dolore culpa. Consequatur itaque debitis magnam expedita, asperiores eos, nisi laborum, omnis atque suscipit doloribus. Debitis possimus ipsam quibusdam vero.
+      
     </>
   )
 }
