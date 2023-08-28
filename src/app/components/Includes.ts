24a2c638245900e22d145449283822182599a30e
MNurIkbal <ikbalmnur17@gmail.com>
2023-08-28 13:48:53 +0700

    ok

---
+++ src/app/components/Includes.ts
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import { Carausel } from "./Carausel";
+export {
+    Navbar,
+    Carausel,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,16 @@
 import type { Config } from 'tailwindcss'
 
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+
+module.exports = withMT({
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
