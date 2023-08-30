1df8b4a2354ffcfeb4a928128ecd210e87563487
MNurIkbal <ikbalmnur17@gmail.com>
2023-08-30 16:48:08 +0700

    pl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import Navbar from './components/Navbar'
 import { Carausel } from './components/Carausel'
 import Link from 'next/link'
+import  Router  from 'next/router'
+import abouts from './page/abouts'
 
 export default function Home() {
   return (
-    <>
-    <div>
-      <Navbar/>
-      <Link href='/halam/login'>login ok</Link>
-    </div>
-    </>
+    <Router>
+       <Route exact path="/" component={Navbar} />
+      <Route path="/about" component={abouts} />
+    </Router>
   )
 }
---
+++ src/app/page/abouts.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function abouts() {
+  return (
+    <div>abouts</div>
+  )
+}
